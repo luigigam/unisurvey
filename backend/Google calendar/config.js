b1e602d139a992ccd6da979864efb20556757bb6
GoldenRose01 <116004785+GoldenRose01@users.noreply.github.com>
2023-05-14 13:52:01 +0000

    fix: fix configs

--- backend/Google calendar/config.js
+++ backend/Google calendar/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    clientId: 801589397940-am3lm7ntgb79igsgk9gmrcg45fl5j003.apps.googleusercontent.com,
-    clientSecret: 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,
-    redirectUri: 'url di reindirizzamento(ex http://localhost:xxxx/oauth2callback' ,
+  clientId:
+    "801589397940-am3lm7ntgb79igsgk9gmrcg45fl5j003.apps.googleusercontent.com",
+  clientSecret: "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",
+  redirectUri:
+    "url di reindirizzamento(ex http://localhost:xxxx/oauth2callback",
 };
