125fe5bdd4632ad531fe180f57ff0e9c93dd0c76
luigigam <luigi.gammino@studenti.unitn.com>
2023-05-16 21:15:41 +0200

    backend patch

--- backend/routes/admins.js
+++ backend/routes/admins.js
@@ -1,120 +1,112 @@
-const express = require('express');
-const router = express.Router();
-const Admin = require('../models/admin');
-const bcrypt = require('bcrypt');
-const hashing = require('../middlewares/encrypt_pssw');
-const jwt = require('jsonwebtoken');
-const authenticateToken = require('../middlewares/authenticateToken');
-const Event = require('../models/event');
+const express = require('express')
+const router = express.Router()
+const Admin = require ('../models/admin')
+const bcrypt = require("bcrypt")
+const hashing = require("../middlewares/encrypt_pssw")
+const jwt = require('jsonwebtoken')
+const authenticateToken = require("../middlewares/authenticateToken")
 
-// Ottieni tutti gli amministratori
-router.get('/', async (req, res) => {
-  try {
-    const admins = await Admin.find();
-    res.json(admins);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve admins' });
-  }
-});
+//Getting all
+router.get('/', async (req,res) => {
+    try {
+        const admins = await Admin.find()
+        res.json(admins)
+    } catch {
+        res.status(500).json({ message: err.message })
+    }
+})
 
-// Ottieni un amministratore specifico
-router.get('/:id', getAdmin, async (req, res) => {
-  res.json(res.admin);
-});
+//Getting one
+router.get('/:id', getAdmin, async (req,res) => {
+    res.json(res.admin)
+})
 
-// Creazione di un nuovo amministratore
-router.post('/', async (req, res) => {
-  const hashed = await hashing(req.body.password);
-  const admin = new Admin({
-    name: req.body.name,
-    surname: req.body.surname,
-    email: req.body.email,
-    password: hashed,
-  });
+//Creating one
+router.post('/', async (req,res) => {
+    const hashed = await hashing(req.body.password)
+    const admin = new Admin({
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        password: hashed
+    })
 
-  try {
-    const newAdmin = await admin.save();
-    res.status(201).json(newAdmin);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+    try {
+        const newAdmin = await admin.save()
+        res.status(201).json(newAdmin)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
 
-// Aggiornamento di un amministratore esistente
-router.patch('/:id', getAdmin, async (req, res) => {
+//Updating one
+router.patch('/:id', getAdmin, async (req,res) => {
   if (req.body.name != null) {
-    res.admin.name = req.body.name;
+    res.admin.name = req.body.name
   }
   if (req.body.surname != null) {
-    res.admin.surname = req.body.surname;
+    res.admin.surname = req.body.surname
   }
   if (req.body.email != null) {
-    res.admin.email = req.body.email;
+    res.admin.email = req.body.email
   }
   if (req.body.password != null) {
-    const hashed = await hashing(req.body.password);
-    res.admin.password = hashed;
+    res.admin.password = req.body.password
   }
   try {
-    const updatedAdmin = await res.admin.save();
-    res.json(updatedAdmin);
+    const updatedAdmin = await res.admin.save()
+    res.json(updatedAdmin)
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(400).json({ message: err.message })
   }
-});
+})
 
-// Eliminazione di un amministratore
-router.delete('/:id', getAdmin, async (req, res) => {
-  try {
-    await res.admin.deleteOne();
-    res.json({ message: 'Deleted Admin' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+// Deleting One
+router.delete("/:id", getAdmin, async (req, res) => {
+	try {
+		await res.admin.deleteOne()
+		res.json({ message: "Deleted Admin" })
+	} catch (err) {
+		res.status(500).json({ message: err.message })
+	}
+})
 
-// Login
+//Login
 router.post('/login', async (req, res) => {
-  const admin = await Admin.findOne({ email: req.body.email });
+  const admin = await Admin.findOne({ email: req.body.email })
   if (admin == null) {
-    return res.status(400).send('Cannot find admin');
+    return res.status(400).send('Cannot find admin')
   }
   try {
     if (await bcrypt.compare(req.body.password, admin.password)) {
-      const accessToken = jwt.sign(admin.toJSON(), process.env.ACCESS_TOKEN_SECRET);
-      res.json({ accessToken: accessToken });
+      const accessToken = jwt.sign(admin.toJSON(), process.env.ACCESS_TOKEN_SECRET)
+      res.json({ accessToken: accessToken })
     } else {
-      res.send('Not allowed');
+      res.send('Not allowed')
     }
   } catch {
-    res.status(500).send();
+    res.status(500).send()
   }
-});
+})
 
-// Home
+//Home
 router.post('/home', authenticateToken, (req, res) => {
-  res.send('Homepage');
-});
-
-// Ottieni tutti gli eventi
-router.get('/events', async (req, res) => {
+  res.send('Homepage')
+})
+  
+async function getAdmin(req, res, next) {
+  let admin
   try {
-    const events = await Event.find();
-    res.json(events);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve events' });
-  }
-});
-
-// Ottieni un evento specifico
-router.get('/events/:id', async (req, res) => {
-  try {
-    const event = await Event.findById(req.params.id);
-    if (!event) {
-      return res.status(404).json({ message: 'Event not found' });
+    admin = await Admin.findById(req.params.id)
+    if (admin == null) {
+      return res.status(404).json({ message: 'Cannot find admin' })
     }
-    res.json(event);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve event' });
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
   }
-});
+
+  res.admin = admin
+  next()
+}
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { google } = require('googleapis');
-const googleConfig = require('./google-config.js');
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
@@ -15,54 +13,13 @@ app.use(express.json());
 
 const studentsRouter = require('./backend/routes/students');
 const adminsRouter = require('./backend/routes/admins');
-const eventsRouter=require('./backend/routes/events');
+//const eventsRouter=require('./backend/routes/events');
 
 
 app.use('/students', studentsRouter);
 app.use('/admins', adminsRouter);
-app.use('/events', eventsRouter);
+//app.use('/events', eventsRouter);
 
 app.use(express.static('frontend'));
-
-const scopes = ['https://www.googleapis.com/auth/calendar'];
-
-const oauth2Client = new google.auth.OAuth2(
-  googleConfig.clientId,
-  googleConfig.clientSecret,
-  googleConfig.redirectUri
-);
-
-const authUrl = oauth2Client.generateAuthUrl({
-  access_type: 'offline',
-  scope: scopes,
-});
-
-app.get('/auth/google', (req, res) => {
-  res.redirect(authUrl);
-});
-
-app.get('/auth/google/callback', async (req, res) => {
-  const { code } = req.query;
-  const { tokens } = await oauth2Client.getToken(code);
-  oauth2Client.setCredentials(tokens);
-  res.redirect('/');
-});
-
-app.get('/events', async (req, res) => {
-  try {
-    const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
-    const { data } = await calendar.events.list({
-      calendarId: 'primary', // ID del calendario desiderato
-      timeMin: new Date().toISOString(),
-      maxResults: 10,
-      singleEvents: true,
-      orderBy: 'startTime',
-    });
-    res.json(data.items);
-  } catch (error) {
-    console.error('Error retrieving events:', error);
-    res.status(500).json({ error: 'Failed to retrieve events' });
-  }
-});
 
 app.listen(3000, () => console.log('Server Started'));
