fb7f8d7feddf374c31db4ec7b8a9d3456652b2b1
luigigam <luigi.gammino@studenti.unitn.com>
2023-06-02 10:29:56 +0200

    minor changes

--- server.js
+++ server.js
@@ -60,25 +60,6 @@ const swaggerOptions = {
 const swaggerSpecs = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 
-const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
-const GOOGLE_PRIVATE_KEY = `-----BEGIN PRIVATE KEY-----\n${process.env.GOOGLE_PRIVATE_KEY}\n-----END PRIVATE KEY-----`;
-const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL;
-const GOOGLE_PROJECT_NUMBER = process.env.GOOGLE_PROJECT_NUMBER;
-const GOOGLE_CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID;
-
-const jwtClient = new google.auth.JWT(
-    GOOGLE_CLIENT_EMAIL,
-    null,
-    GOOGLE_PRIVATE_KEY,
-    SCOPES
-);
-  
-const calendar = google.calendar({
-    version: 'v3',
-    project: GOOGLE_PROJECT_NUMBER,
-    auth: jwtClient
-});
-
 app.get('/', (req, res) => {
     calendar_constants.calendar.events.list({
       calendarId: calendar_constants.GOOGLE_CALENDAR_ID,
@@ -98,47 +79,6 @@ app.get('/', (req, res) => {
       }
     });
   });
-
-app.get("/createEvent",(req,res)=>{
-    var event = {
-      'summary': 'Evento Test!',
-      'location': 'Povo',
-      'description': 'Test evento aggiunto al calendario!',
-      'start': {
-        'dateTime': '2023-07-12T09:00:00-07:00'
-      },
-      'end': {
-        'dateTime': '2023-07-12T17:00:00-07:00'
-      },
-      'attendees': [],
-      'reminders': {
-        'useDefault': false,
-        'overrides': [
-          {'method': 'email', 'minutes': 24 * 60},
-          {'method': 'popup', 'minutes': 10},
-        ],
-      },
-    };
-      
-    const auth = new google.auth.GoogleAuth({
-      keyFile: './google-calendar-key-path.json',
-      scopes: 'https://www.googleapis.com/auth/calendar',
-    });
-    auth.getClient().then(a=>{
-      calendar_constants.calendar.events.insert({
-        auth:a,
-        calendarId: calendar_constants.GOOGLE_CALENDAR_ID,
-        resource: event,
-      }, function(err, event) {
-        if (err) {
-          console.log('There was an error contacting the Calendar service: ' + err);
-          return;
-        }
-        console.log('Event created: %s', event.data);
-        res.jsonp("Event successfully created!");
-      });
-    })
-})
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
@@ -169,34 +109,7 @@ app.get('/login', (req, res)=>{
   res.sendFile(path.join(__dirname, './frontend/static/Loginpages/login.html'));
 });
 
-const Student = require('./backend/models/student');
-const validateEmail = require('./backend/middlewares/validateEmail');
-const hashing = require('./backend/middlewares/encrypt_pssw');
 
-app.post('/api/students/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    // Verifica se l'email esiste nel database
-    const student = await Student.findOne({ email });
-    if (!student) {
-      return res.status(404).json({ message: 'Email not found' });
-    }
-
-    // Verifica se la password è corretta
-    const isPasswordCorrect = await bcrypt.compare(password, student.password);
-    if (!isPasswordCorrect) {
-      return res.status(401).json({ message: 'Wrong password' });
-    }
-
-    // Accesso consentito, restituisci una risposta di successo
-    return res.status(200).json({ message: 'Login successful' });
-  } catch (error) {
-    // Errore del server durante la verifica
-    console.error('Error during login:', error);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-});
 
 app.get('/api/aule', (req, res) => {
   Classroom.find({}, (err, aule) => {
